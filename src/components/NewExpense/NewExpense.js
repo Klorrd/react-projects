bfb52339496f983ab0269e507c14dab3c512eef8
Kalindu <kalindu.mk@gmail.com>
2022-12-29 15:49:02 +0530

    1

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,10 +1,12 @@
 import React from "react";
+import ExpenseForm from "./ExpenseForm";
+
 const NewExpense = () => {
   return (
     <div className={"new-expense"}>
-      >
+      <ExpenseForm />
     </div>
-  )
-}
+  );
+};
 
 export default NewExpense;
